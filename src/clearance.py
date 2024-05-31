2b6860cace39eea266abb1e1f891b211366031a5
Charge <expidite@riseup.net>
2024-05-30 22:06:04 -0500

    modified

--- src/clearance.py
+++ src/clearance.py
@@ -10,6 +10,7 @@ import discord_handler
 class ScrapeCatagory:
     def __init__(self) -> None:
         self.products_list = [ ]
+        self.sale_products_list = [ ]
         self.headers = {'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7', 'accept-language': 'en-US,en;q=0.9','cache-control': 'max-age=0', 'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"', 'sec-ch-ua-mobile': '?1', 'sec-ch-ua-platform': '"Android"', 'sec-fetch-dest': 'document', 'sec-fetch-mode': 'navigate', 'sec-fetch-site': 'none', 'sec-fetch-user': '?1', 'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Mobile Safari/537.36'}
         self.cookies = {'eCookieId': f"{self.generate_cookie_id()}",}
 
@@ -21,8 +22,8 @@ class ScrapeCatagory:
 
 
 
-    def main_handler(self, base_catagory_url: str, api_url: str) -> None:
-        print(f"[Autozone] Starting Scrape on {base_catagory_url}")
+    def main_handler(self) -> None:
+        print(f"[Autozone] Starting Scrape on Clearance")
         # Send the inital request to the base catagory url
         with open("../database/proxies.txt", "r") as proxies_file:
             proxies_list = proxies_file.readlines()
@@ -33,7 +34,7 @@ class ScrapeCatagory:
         }
 
         try:
-            inital_request = requests.get(api_url.replace("[PAGENUMBER]","1"), headers=self.headers, cookies=self.cookies, proxies=resources_proxy_dict, timeout=10)
+            inital_request = requests.get(f"https://www.autozone.com/external/product-discovery/browse-search/v1/product-shelves?country=USA&customerType=B2C&salesChannel=ECOMM&preview=false&canonicalPath=%2Fdeals%2Fclearance&pageNumber=1&recordsPerPage=48&storeId=4973&ruleTags=CLEARANCE&isLandingPage=true", headers=self.headers, cookies=self.cookies, proxies=resources_proxy_dict, timeout=10)
         except Exception as e:
             print(e)
         # Add the products from the inital request to the products list
@@ -46,20 +47,54 @@ class ScrapeCatagory:
         if total_pages > 10: total_pages = 10
         # Iterate through the pages gathering products
         for page in range(2, total_pages + 2):
-            products = self.request_api_base(api_url.replace("[PAGENUMBER]", str(page)), self.headers)
+            products = self.request_api_base(f"https://www.autozone.com/external/product-discovery/browse-search/v1/product-shelves?country=USA&customerType=B2C&salesChannel=ECOMM&preview=false&canonicalPath=%2Fdeals%2Fclearance&pageNumber={page}&recordsPerPage=48&storeId=4973&ruleTags=CLEARANCE&isLandingPage=true", self.headers)
             time.sleep(2)
 
             if products == None: print(f"{page} Has Unavalible Products")
             else: self.save_product_data(products)
 
-            print(f"{page}/{total_pages+1}")
+            print(f"Scraping Page {page}/{total_pages+1}")
         
-        if self.generate_product_pricing() == None:
-            pass
-        else:
-            print(f"[Autozone] Uploading {len(products)} Products to Database")
-            for product in self.products_list:
-                database.save_product(product)
+        self.generate_product_pricing()
+        print(f"[Autozone] Found {len(self.sale_products_list)} Products on Clearance")
+
+        self.check_duplicates()
+
+        for product in self.sale_products_list:
+            discord_handler.send_webhook(product)
+    
+
+
+
+
+    def check_duplicates(self) -> None:
+        """This function checks for duplicates in the products list and removes them"""
+        with open("../database/sent_products.json", "r") as saved_products_file:
+            saved_products_data = json.load(saved_products_file)
+        
+        skus_list = saved_products_data.keys()
+
+        for product in self.sale_products_list:
+            if product["sku"] in skus_list:
+
+                if saved_products_data[product["sku"]] == product["price"]:
+                    self.sale_products_list.remove(product)
+                    print(f"[Autozone] Product {product['sku']} Already Exists | Same Price | Removing")
+
+                elif saved_products_data[product["sku"]] != product["price"]:
+                    saved_products_data[product["sku"]] = product["price"]
+                    print(f"[Autozone] Product {product['sku']} Already Exists | Different Price | Updating")
+
+            else:
+                saved_products_data[product["sku"]] = product["price"]
+                print(f"[Autozone] Product {product['sku']} Added to Database")
+        
+        with open("../database/sent_products.json", "w") as saved_products_file:
+            json.dump(saved_products_data, saved_products_file, indent=4)
+
+
+
+
 
     def generate_product_pricing(self) -> None:
         """This function makes calls to the product pricing API to get the pricing for each product"""
@@ -75,34 +110,26 @@ class ScrapeCatagory:
         product_skus_querry = [product["sku"] for product in self.products_list]
         
         pricing_api_url = f"https://www.autozone.com/ecomm/b2c/v1/browse/skus/price/{','.join(product_skus_querry)}?storeNumber=4461"
-        pricing_response = requests.get(pricing_api_url, headers=self.headers, cookies=self.cookies, proxies=resources_proxy_dict, timeout=10)
-        
-        if "Internal Server Error" in pricing_response.text:
-            print("Internal Server Error")
-            return None
-        elif "Access Denied" in pricing_response.text:
-            print("Access Denied")
-            return None
 
-        print("[Auzozone] Requesting Pricing")
-        try:
+        # Batch the product skus into groups of 24
+        batched_skus = [product_skus_querry[i:i+24] for i in range(0, len(product_skus_querry), 24)]
 
-            pricing_json = pricing_response.json()
-        except:
-            print("No Pricing Json Data")
-            print(pricing_response.text)
-            return None
-        try:
+        # Iterate through each batch and make the pricing API request
+        for batch in batched_skus:
+            pricing_api_url = f"https://www.autozone.com/ecomm/b2c/browse/v3/skus/price-availability/{','.join(batch)}"
+            pricing_response = requests.get(pricing_api_url, headers=self.headers, cookies=self.cookies, proxies=resources_proxy_dict, timeout=10)
 
-            for index, product in enumerate(pricing_json):
-                if self.products_list[index]["sku"] == product["skuId"]:
-                    self.products_list[index]["price"] = product["retailPrice"]
-                else:
-                    print("Error indexing price")
-        except:
-            print(pricing_response.text)
-        
-        return self.products_list
+            for index, product in enumerate(pricing_response.json()):
+                for p in self.products_list:
+                    if p["sku"] == product['skuPricingAndAvailability']["skuId"]:
+                        p["price"] = product['skuPricingAndAvailability']["retailPrice"]
+                        
+                        if not product['skuPricingAndAvailability']['wasRetailPrice'] == None:
+                            p['old_price'] = product['skuPricingAndAvailability']['wasRetailPrice']
+                            p['shipping_quantity'] = product['availabilityInfoVO']['sthQuantity']
+                            self.sale_products_list.append(p)
+            time.sleep(2)
+
                 
 
     def save_product_data(self, product_data: List[dict]) -> None:
@@ -137,17 +164,10 @@ class ScrapeCatagory:
         api_data = api_data["productShelfResults"]["skuRecords"]
 
         return api_data
-        
 
 
-database = database.DatabaseHandler()
 
-while True:
-
-    with open("catagory_api_urls.json", "r") as json_file:
-        json_data = json.load(json_file)
-
-    data = random.choice(json_data)
+ScrapeCatagory().main_handler()
 
 
-    ScrapeCatagory().main_handler(data["original_path"], data["api_url"])
+# https://www.autozone.com/external/product-discovery/browse-search/v1/product-shelves?country=USA&customerType=B2C&salesChannel=ECOMM&preview=false&canonicalPath=%2Fdeals%2Fclearance&pageNumber=1&recordsPerPage=24&storeId=4973&ruleTags=CLEARANCE&isLandingPage=true
--- src/discord_handler.py
+++ src/discord_handler.py
@@ -1,20 +1,38 @@
 from discord_webhook import DiscordWebhook, DiscordEmbed
+import time
+
+
+def calculate_discount( original_price: float, sale_price: float ) -> float:
+    deal_price_difference = round(float(float(original_price) - float(sale_price)), 2)
+    deal_percent_off = float(deal_price_difference)/float(original_price)
+    discount = round(deal_percent_off*100)
+    return discount
 
 
 
+def send_webhook( product: list ) -> None:
+    discount = calculate_discount(product['old_price'], product['price'])
 
+    if int(discount) > 0 and int(discount) <= 49:
+        webhook = "https://discord.com/api/webhooks/1245064631446278174/qoOZ8GsI589Mo3jc0-1mpQ8pnfk1yeZ9ovW3aOJ8M1VUrPB8ZYRF2Xmv6n3HrUJuB4H8"
+    elif int(discount) > 50 and int(discount) <= 69:
+        webhook = "https://discord.com/api/webhooks/1245064805413294131/Pr_okVT19I_VxRAI6YjWMJGcLPtylnphkhgxM7agoGEWmBj-0Svg8Vqb9806ST4TRcQW"
+    elif int(discount) > 70:
+        webhook = "https://discord.com/api/webhooks/1245064904432554128/n7qxm6gHBQNFivKwQmOg270Qc_8kY6MY2y7nn1n5-qFizhhm6aDR9u4ncLOU7brVyEof"
+    else:
+        webhook = "https://discord.com/api/webhooks/1245063761224667276/_0ZHc_lJyJaHdSzJ9Zs5OchgX_uA5xUV-e8qgmEQEX1ATsel6lxAlFtDKmLUlWCMgTHk"
 
-
-def send_zero_matches_backend( product: dict ) -> None:
-    webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1227630360553066526/nUAxToDsEeUglOauRvA03DbmtcIw_J7J9VRgOsB77AGR14cr-WigowSCwmz-k55venWF")
-    print(product['name'])
-    embed = DiscordEmbed(title=product['name'], color=0xF5851B)
-    embed.add_embed_field(name="Price", value=f"{product['price']}")
-    embed.add_embed_field(name="SKU", value=f"``{product['sku']}``")
+    webhook = DiscordWebhook(url=webhook)
+    embed = DiscordEmbed(title=product['name'], color=0xF5851B, url = f"https://www.autozone.com/searchresult?searchText={product['sku']}")
+    embed.add_embed_field(name="Original Price", value=f"$~~{product['old_price']}~~")
+    embed.add_embed_field(name="Price", value=f"${product['price']}")
+    embed.add_embed_field(name="Discount", value=f"{discount}%")
     embed.add_embed_field(name="Part Number", value=f"``{product['part_number']}``")
-    print(product['image'])
-    embed.set_image(url=product['image'])
-    embed.set_footer(text="AutoParts Monitor Powered by PriceErrors")
+    embed.add_embed_field(name="SKU", value=f"``{product['sku']}``")
+    embed.add_embed_field(name="Online Stock", value=f"``{product['shipping_quantity']}``")
+    embed.set_thumbnail(url=product['image'].replace("http", "https"))
+    embed.set_footer(text="AutoZone Monitor Powered by PriceErrors")
     webhook.add_embed(embed)
 
     response = webhook.execute()
+    time.sleep(1)
